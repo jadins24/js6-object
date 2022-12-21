fccef38d49bcf51f084abc5151c25bfc5b01c5ae
jadins24 <jadinesh04@gmail.com>
2022-12-21 11:25:51 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var mobile = {type:"vivo", model:"v20pro", color:"white" ,price:"30000"};
+var waterbottle = {type:"sanware", model:"clasic", color:"blue", price:"300"};
+var bag = {type:"backbag", model:"torist", color:"blue" ,price:"1000"};
+var bike = {type:"sport", model:"rr310", color:"red", price:"300000"};
+var bike = {type:"", model:"", color:"", price:""};
+
